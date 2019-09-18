ff851032a372e04a74cc25de21d7f34675959efc
Kelvin <Mr.Secret0987@gmail.com>
2019-09-18 15:11:16 +0700

    Fix: complete two first rows

--- src/Form.js
+++ src/Form.js
@@ -16,30 +16,29 @@ class Form extends React.Component{
 			}
 
 			}
+ 
 		render(){
 			return(
 				<form>
-				<h1>Phiếu ứng tuyển việc làm</h1>
-				<span>Ảnh
-				<button className="UserPic" value={this.state.picture}>
-				<i class="fas fa-camera"></i>
-				</button>
-				</span>
-				<input placeholder="Họ tên" className='fullname' type='text'/>
-				<div>
-				<span>
-				<select className='gender' value={this.state.gender}>
-					<option value = '0'>Nam</option>
-					<option value = '1'>Nữ</option>
-				</select>
-				<label>
-				Ngày sinh
-				</label>
-				<input type = 'date' className='dob' />
-				</span>
-				</div>
+					<h1>Phiếu ứng tuyển việc làm</h1>
+					<span>Ảnh
+						<input className="UserPic" value={this.state.picture} type = 'file' id='userPic'/>
+					</span>
+					<input placeholder="Họ tên" className='fullname' type='text'/>
+					<div>
+						<span>
+							<select className='gender' value={this.state.gender}>
+								<option value = '0'>Nam</option>
+								<option value = '1'>Nữ</option>
+							</select>
+							<label>
+							Ngày sinh
+							</label>
+							<input type = 'date' className='dob' />
+						</span>
+					</div>
 				</form>
-				)
+				);
 	}
 }
 export default Form;
