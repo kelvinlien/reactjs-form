756c1b78a0f3f1ca82f3e50aae6fd5245a0ded04
Kelvin <Mr.Secret0987@gmail.com>
2019-09-19 23:14:32 +0700

    Fea: data experience checkboxes

---
+++ src/DataCheckBox.js
@@ -0,0 +1,39 @@
+import React from 'react';
+class DataCheckBox extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(
+		<form>
+			<h1>Kinh nghiệm về dữ liệu</h1>
+			<div>
+				<input type = 'checkbox' className = 'interviewer'/>
+				<label>Đã từng làm điều tra viên/phỏng vấn viên.</label>
+			</div>
+			<div>
+				<input type = 'checkbox' className = 'supervisor'/>
+				<label>Đã từng làm giám sát viên.</label>
+			</div>
+			<div>
+				<input type = 'checkbox' className = 'tester'/>
+				<label>Đã từng kiểm thử (test) bảng hỏi, góp ý/đề xuất điều chỉnh nội dung bảng hỏi giấy</label>
+			</div>
+			<div>
+				<input type = 'checkbox' className = 'statistic'/>
+				<label>Đã từng nhập liệu sử dụng các phần mềm thống kê (Excel/SPSS/Stata/…)</label>
+			</div>
+			<div>
+				<input type = 'checkbox' className = 'analytic'/>
+				<label>Đã từng phân tích dữ liệu sử dụng các công cụ phân tích (Excel/Stata/R/…)</label>
+			</div>
+			<div>
+				<input type = 'checkbox' className = 'none'/>
+				<label>Chưa có kinh nghiệm nào.</label>
+			</div>
+
+
+		</form>
+		);
+	}
+}export default DataCheckBox;
